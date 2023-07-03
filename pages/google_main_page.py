34d2062e7d264c26379e2c03691e653a899e95fe
ivan <ivan.khruchshev@gmail.com>
2023-07-03 17:44:57 +0500

    Added allure reports

--- pages/google_main_page.py
+++ pages/google_main_page.py
@@ -1,3 +1,4 @@
+import allure
 from selene.support.shared import browser
 from selene import be, have
 
@@ -10,20 +11,33 @@ class GoogleMainPage:
     def open(self):
         browser.open(self.url)
 
+    @allure.step("Click to search input")
     def click_to_search_input(self):
         browser.element(self.search_input_locator).click()
 
+    @allure.step("Fill search input with text")
     def fill_search_input(self, text):
         browser.element(self.search_input_locator).type(text)
 
+    @allure.step("Click to search input cross")
     def clear_search_input(self):
         browser.element(self.search_input_cross_locator).click()
 
+    @allure.step("Search input is empty")
     def search_input_is_empty(self):
         return browser.element(self.search_input_locator).matching(be.blank)
 
+    @allure.step("Search input is not empty")
+    def search_input_is_not_empty(self):
+        return browser.element(self.search_input_locator).matching(be.not_.blank)
+
+    @allure.step("Search input cross is visible")
     def search_input_have_cross(self):
         return browser.element(self.search_input_cross_locator).matching(be.visible)
+
+    @allure.step("Search input cross is not visible")
+    def search_input_have_not_cross(self):
+        return browser.element(self.search_input_cross_locator).matching(be.not_.visible)
 
     def search_input_history_list_is_visible(self):
         pass
--- tests/google_main_page_test.py
+++ tests/google_main_page_test.py
@@ -1,30 +1,36 @@
+import allure
 from pages.google_main_page import GoogleMainPage
 from faker import Faker
 
 
+@allure.suite("Google Main Page")
 class TestGoogleMainPage:
-    Faker.seed()
-    faker_item = Faker("ru_RU")
-    search_text = faker_item.text(max_nb_chars=40)
+    @allure.feature("Google Main Page Input")
+    class TestGoogleMainPageInput:
+        Faker.seed()
+        faker_item = Faker("ru_RU")
+        search_text = faker_item.text(max_nb_chars=40)
 
-    def test_empty_search_box(self):
-        google_main_page = GoogleMainPage()
-        google_main_page.open()
-        assert google_main_page.search_input_is_empty() is True
-        assert google_main_page.search_input_have_cross() is False
+        @allure.title("Empty Search Input Test")
+        def test_empty_search_box(self):
+            google_main_page = GoogleMainPage()
+            google_main_page.open()
+            assert google_main_page.search_input_is_empty()
+            assert google_main_page.search_input_have_not_cross()
 
-    def test_filled_search_box(self):
-        google_main_page = GoogleMainPage()
-        google_main_page.open()
-        google_main_page.fill_search_input(text=self.search_text)
-        assert google_main_page.search_input_is_empty() is False
-        assert google_main_page.search_input_have_cross() is True
+        @allure.title("Filled Search Input Test")
+        def test_filled_search_box(self):
+            google_main_page = GoogleMainPage()
+            google_main_page.open()
+            google_main_page.fill_search_input(text=self.search_text)
+            assert google_main_page.search_input_is_not_empty()
+            assert google_main_page.search_input_have_cross()
 
-    def test_clear_search_box(self):
-        google_main_page = GoogleMainPage()
-        google_main_page.open()
-        google_main_page.fill_search_input(text=self.search_text)
-        google_main_page.clear_search_input()
-        assert google_main_page.search_input_is_empty() is True
-        assert google_main_page.search_input_have_cross() is False
-
+        @allure.title("Cleared Search Input Test")
+        def test_clear_search_box(self):
+            google_main_page = GoogleMainPage()
+            google_main_page.open()
+            google_main_page.fill_search_input(text=self.search_text)
+            google_main_page.clear_search_input()
+            assert google_main_page.search_input_is_empty()
+            assert google_main_page.search_input_have_not_cross()
